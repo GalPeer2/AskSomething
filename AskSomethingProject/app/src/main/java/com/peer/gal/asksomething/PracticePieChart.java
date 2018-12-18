3ecb43513d5413ba097775e4a2e8a21f4e8b412d
galpeer2 <galpeer2@gmail.com>
2018-12-18 11:20:35 +0200

    trying to fiv the pie + working in school now

--- AskSomethingProject/app/src/main/java/com/peer/gal/asksomething/PracticePieChart.java
+++ AskSomethingProject/app/src/main/java/com/peer/gal/asksomething/PracticePieChart.java
@@ -28,7 +28,7 @@ public class PracticePieChart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_pie_chart);
-
+/*
         theStateMgr = new StateMgr (this );
         asklSomeThingState= theStateMgr.LoadState();
         user=asklSomeThingState.getDictionary().get(asklSomeThingState.getUserName());
@@ -39,29 +39,47 @@ public class PracticePieChart extends AppCompatActivity {
         if (extras != null) {
             thePlace =Integer.parseInt(extras.getString("thePlace"));
         }
+
+
+
         theQuestionDealed =user.getMyHistoryQuestions().get(thePlace);
+        */
+      theQuestionDealed=  new Question("????","a","b","c","d");
+        ArrayList<String> s = new ArrayList<String>();
+        s.add("str1");
+        s.add("str hello");
+        s.add("str bye");
+        theQuestionDealed.setVotersForAns1(s);
+        ArrayList<String> s2 =new ArrayList<>();
+        s2.add("fgf");
+        theQuestionDealed.setVotersForAns2(s2);
+       // theQuestionDealed.setVotersForAns3(s2);
+
         PieChartView pieChartView = (PieChartView)findViewById(R.id.chart);
         List<SliceValue> pieData = new ArrayList<>();
-/*
-        pieData.add(new SliceValue(15, Color.BLUE));
-        pieData.add(new SliceValue(25, Color.GRAY));
-        pieData.add(new SliceValue(10, Color.RED));
-        pieData.add(new SliceValue(60, Color.MAGENTA));
 
-*/
         int v1=theQuestionDealed.getVotersForAns1().size();
         int v2=theQuestionDealed.getVotersForAns2().size();
         int v3=theQuestionDealed.getVotersForAns3().size();
         int v4=theQuestionDealed.getVotersForAns4().size();
-        int bonus=0;
-        if ((v1+v2+v3+v4==0))
-             bonus++;
+
+        v1=3;
+        v2=0;
+        v3=0;
+        v4=0;
 
 
-        pieData.add(new SliceValue((v1+bonus)/(v1+v2+v3+v4+bonus), Color.BLUE).setLabel(theQuestionDealed.getAns1()+" : "+v1+" voters"));
-        pieData.add(new SliceValue((v2+bonus)/(v1+v2+v3+v4+bonus), Color.GRAY).setLabel(theQuestionDealed.getAns2()+" : "+v2+" voters"));
-        pieData.add(new SliceValue((v3+bonus)/(v1+v2+v3+v4+bonus), Color.RED).setLabel(theQuestionDealed.getAns3()+" : "+v3+" voters"));
-        pieData.add(new SliceValue((v4+bonus)/(v1+v2+v3+v4+bonus), Color.MAGENTA).setLabel(theQuestionDealed.getAns4()+" : "+v4+" voters"));
+    if (v1!=0)
+        pieData.add(new SliceValue((v1)/(v1+v2+v3+v4)*100, Color.BLUE).setLabel(theQuestionDealed.getAns1()+" : "+v1+" voters"));
+        if (v2!=0)
+        pieData.add(new SliceValue((v2)/(v1+v2+v3+v4)*100, Color.GRAY).setLabel(theQuestionDealed.getAns2()+" : "+v2+" voters"));
+        if (v3!=0)
+        pieData.add(new SliceValue((v3)/(v1+v2+v3+v4)*100, Color.RED).setLabel(theQuestionDealed.getAns3()+" : "+v3+" voters"));
+       if (v4!=0)
+        pieData.add(new SliceValue((v4)/(v1+v2+v3+v4)*100, Color.MAGENTA).setLabel(theQuestionDealed.getAns4()+" : "+v4+" voters"));
+
+
+       pieData.add(new SliceValue(100,Color.RED).setLabel("kkkaka"));
 
 
         PieChartData pieChartData = new PieChartData(pieData);
@@ -73,6 +91,9 @@ public class PracticePieChart extends AppCompatActivity {
 
 
         pieChartView.setPieChartData(pieChartData);
+
+
+
 
     }
 }
