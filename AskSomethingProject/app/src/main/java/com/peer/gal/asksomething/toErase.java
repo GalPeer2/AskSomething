edfc87f12712faafba830329055c8c3308f4ef93
galpeer2 <galpeer2@gmail.com>
2018-12-19 12:32:21 +0200

    OnSwipeTouchListener.java

---
+++ AskSomethingProject/app/src/main/java/com/peer/gal/asksomething/toErase.java
@@ -0,0 +1,13 @@
+package com.peer.gal.asksomething;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class toErase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_erase);
+    }
+}
