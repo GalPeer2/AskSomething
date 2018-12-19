da5e0895f6f1423f1e3962fc892c95935045d682
galpeer2 <galpeer2@gmail.com>
2018-12-19 17:19:04 +0200

    PracticePieChart.java pichifx (hole and onTouch)

--- AskSomethingProject/app/src/main/java/com/peer/gal/asksomething/PracticePieChart.java
+++ AskSomethingProject/app/src/main/java/com/peer/gal/asksomething/PracticePieChart.java
@@ -18,6 +18,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import lecho.lib.hellocharts.listener.PieChartOnValueSelectListener;
 import lecho.lib.hellocharts.model.PieChartData;
 import lecho.lib.hellocharts.model.SliceValue;
 import lecho.lib.hellocharts.view.PieChartView;
@@ -43,7 +44,6 @@ public class PracticePieChart extends AppCompatActivity {
         pieChartView = (PieChartView) findViewById(R.id.chart);
         linearLayout=(LinearLayout)findViewById(R.id.theBigLayout) ;
 
-        List<SliceValue> pieData = new ArrayList<>();
         theStateMgr = new StateMgr(this);
         asklSomeThingState = theStateMgr.LoadState();
         user = asklSomeThingState.getDictionary().get(asklSomeThingState.getUserName());
@@ -62,6 +62,7 @@ public class PracticePieChart extends AppCompatActivity {
 
 
         theQuestionDealed = user.getMyHistoryQuestions().get(thePlace);
+        LoadQuestion(theQuestionDealed);
 
         //exampleQuestion
    /*   theQuestionDealed=  new Question("????","a","b","c","d");
@@ -76,48 +77,22 @@ public class PracticePieChart extends AppCompatActivity {
         theQuestionDealed.setVotersForAns3(s2);*/
 
 
-        int v1 = theQuestionDealed.getVotersForAns1().size();
-        int v2 = theQuestionDealed.getVotersForAns2().size();
-        int v3 = theQuestionDealed.getVotersForAns3().size();
-        int v4 = theQuestionDealed.getVotersForAns4().size();
 
-
-        if (v1 != 0)
-            pieData.add(new SliceValue((100 * v1) / (v1 + v2 + v3 + v4), Color.BLUE).setLabel(theQuestionDealed.getAns1() + " : " + v1 + " voters"));
-        if (v2 != 0)
-            pieData.add(new SliceValue((v2 * 100) / (v1 + v2 + v3 + v4), Color.GRAY).setLabel(theQuestionDealed.getAns2() + " : " + v2 + " voters"));
-        if (v3 != 0)
-            pieData.add(new SliceValue((v3 * 100) / (v1 + v2 + v3 + v4), Color.RED).setLabel(theQuestionDealed.getAns3() + " : " + v3 + " voters"));
-        if (v4 != 0)
-            pieData.add(new SliceValue((v4 * 100) / (v1 + v2 + v3 + v4), Color.GREEN).setLabel(theQuestionDealed.getAns4() + " : " + v4 + " voters"));
-
-
-        PieChartData pieChartData = new PieChartData(pieData);
-
-        pieChartData.setHasLabels(true);
-
-        pieChartData.setHasLabels(true).setValueLabelTextSize(14);
-
-        pieChartView.setPieChartData(pieChartData);
-
-        linearLayout.setOnTouchListener(new OnSwipeTouchListener() {
+        linearLayout.setOnTouchListener(new OnSwipeTouchListener(this) {
 
             public void onSwipeRight() {
                 if (thePlace==0)
                     return;
-                Intent a =new Intent(PracticePieChart.this,PracticePieChart.class);
-                a.putExtra("thePlace",thePlace-1);
-                finish();
-                startActivity(a);
+                thePlace--;
+                LoadQuestion(user.getMyHistoryQuestions().get(thePlace));
+
 
             }
             public void onSwipeLeft() {
                 if (thePlace==user.getMyHistoryQuestions().size()-1)
                     return;
-                Intent a =new Intent(PracticePieChart.this,PracticePieChart.class);
-                a.putExtra("thePlace",thePlace+1);
-                finish();
-                startActivity(a);
+                thePlace++;
+                LoadQuestion(user.getMyHistoryQuestions().get(thePlace));
             }
 
 
@@ -125,6 +100,20 @@ public class PracticePieChart extends AppCompatActivity {
                 return gestureDetector.onTouchEvent(event);
             }
         });
+
+        pieChartView.setOnValueTouchListener(new PieChartOnValueSelectListener() {
+            @Override
+            public void onValueSelected(int arcIndex, SliceValue value) {
+                Toast.makeText(getActivity(), String.valueOf(value.getLabelAsChars()), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onValueDeselected() {
+
+            }
+        });
+
+
 
 
 
@@ -182,5 +171,37 @@ public class PracticePieChart extends AppCompatActivity {
             }
 
         }
+    }
+
+    public void LoadQuestion(Question theQuestionDealed)
+    {
+        List<SliceValue> pieData = new ArrayList<>();
+        int v1 = theQuestionDealed.getVotersForAns1().size();
+        int v2 = theQuestionDealed.getVotersForAns2().size();
+        int v3 = theQuestionDealed.getVotersForAns3().size();
+        int v4 = theQuestionDealed.getVotersForAns4().size();
+
+
+        if (v1 != 0)
+            pieData.add(new SliceValue((100 * v1) / (v1 + v2 + v3 + v4), Color.BLUE).setLabel(theQuestionDealed.getAns1() + " : " + v1 + " voters"));
+        if (v2 != 0)
+            pieData.add(new SliceValue((v2 * 100) / (v1 + v2 + v3 + v4), Color.GRAY).setLabel(theQuestionDealed.getAns2() + " : " + v2 + " voters"));
+        if (v3 != 0)
+            pieData.add(new SliceValue((v3 * 100) / (v1 + v2 + v3 + v4), Color.RED).setLabel(theQuestionDealed.getAns3() + " : " + v3 + " voters"));
+        if (v4 != 0)
+            pieData.add(new SliceValue((v4 * 100) / (v1 + v2 + v3 + v4), Color.GREEN).setLabel(theQuestionDealed.getAns4() + " : " + v4 + " voters"));
+
+
+        PieChartData pieChartData = new PieChartData(pieData);
+
+        pieChartData.setHasLabels(true);
+
+        pieChartData.setHasLabels(true).setValueLabelTextSize(14);
+
+        pieChartData.setHasCenterCircle(true).setCenterText1(theQuestionDealed.getQue());
+        pieChartData.setCenterText1FontSize(12).setCenterCircleScale(200);// size of the middle
+
+        pieChartView.setPieChartData(pieChartData);
+
     }
 }
